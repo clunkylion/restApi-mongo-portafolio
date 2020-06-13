48539c99679e0798be69830f0c69cdc748db5306
Jose <cote99salamanca@gmail.com>
2020-06-13 09:34:10 -0400

    se agregaron los headers para evitar errores con los navegadores

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import '@babel/polyfill';
 import app from './server';
 async function main(){
     await app.listen(app.get('port'));
-    console.log('Server on Port ', app.get('port'))
+    console.log('Server on Port ', app.get('port'));
+    
 }
 main();
--- src/server.js
+++ src/server.js
@@ -4,12 +4,22 @@ const app = express();
 app.set('port', process.env.PORT || 4000)
 
 //middleware (se ejecutan antes de que llegen a las rutas)
+app.use(urlencoded({
+    extended: false
+}));
 app.use(json())
-
+app.use(function (req,res,next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+})
 
 //call ruotes
 import indexRoutes from './routes/index.routes';
 import contactRouter from './routes/contactForm.routes';
+import { urlencoded } from 'body-parser';
 //use routes
 app.use(indexRoutes);
 app.use('/contacto',contactRouter);
